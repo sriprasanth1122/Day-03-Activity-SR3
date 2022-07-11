ab7192c1aabbf3b39bac531ee6d733f540616f83
Sri Prasanth <107243584+sriprasanth1122@users.noreply.github.com>
2022-07-11 10:13:07 +0530

    Update json(resume).js

--- json(resume).js
+++ json(resume).js
@@ -11,7 +11,7 @@ let resume=
          },
          techanicalskill:{
 
-             SoftwarestoOperate :'AutoCAD,Basic knowledge in HTML,CSS,JavaScript';
+             SoftwarestoOperate :'AutoCAD,Basic knowledge in HTML,CSS,JavaScript',
             
 
          },
